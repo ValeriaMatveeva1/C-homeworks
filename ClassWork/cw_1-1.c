5c43638f2c6b2e3ebaa12773d5f4f629ae0ef201
ValeriaMatveeva1 <matveevavalerochka11@gmail.com>
2022-02-11 11:18:03 +0300

    2 task

--- ClassWork/cw_1-1.c
+++ ClassWork/cw_1-1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <limits.h>
 
+// 1.Суммирование последовательности целыхь чисел, состоящей из n чисел
 void sum(int n) {
     int num;
     int sum = 0;
@@ -9,17 +11,41 @@ void sum(int n) {
         sum += num;
         n--;
     }
-    printf("%d", sum);
+    printf("%d\n", sum);
+}
+
+// 2. Нахождение минимума из последовательности чисел, состоящей из n чисел
+void min(int n) {
+    int num;
+    int res = INT_MAX;
+    while (n != 0) {
+        printf("Enter %d number: ", n);
+        scanf("%d", &num);
+        if (num < res) {
+            res = num;
+        }
+        n--;
+    }
+    printf("%d", res);
 }
 
 int main() {
-    int a;
+    int a; // 1
     printf("Enter a number of numbers to sum: ");
     if (scanf("%d", &a) == 1) { // проверка на правильность вводимого аргумента
         sum(a);
     } else {
         printf("Not an allowed type!\n");
     }
+
+    int b; // 2
+    printf("Enter a number of numbers to find a minimum: ");
+    if (scanf("%d", &b) == 1) { // проверка на правильность вводимого аргумента
+        min(b);
+    } else {
+        printf("Not an allowed type!\n");
+    }
+    
 
     return 0;
 }
